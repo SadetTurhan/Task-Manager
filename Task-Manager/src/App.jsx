6904f73c0eaa1a36c5ca352c3eb7b74d91175d52
Sadet Turhan <msadetturhan1@gmail.com>
2023-08-06 22:48:50 +0300

    working on authorization

--- Task-Manager/src/App.jsx
+++ Task-Manager/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
+import { Routes, Route } from "react-router-dom"
 import './App.css'
 import { LoginForm } from "./components/LoginForm"
-
+import { ProfilePage } from "../src/components/ProfilePage"
 function App() {
 
   return (
-    <LoginForm />
+   <Routes>
+      <Route path="/" element={<LoginForm />} />
+      <Route path="/profile" element={<ProfilePage />} />
+   </Routes>
   )
 }
 
---
+++ Task-Manager/src/utils/auth.jsx
@@ -0,0 +1,13 @@
+import { useState,createContext } from "react";
+
+const AuthContext = createContext(null)
+
+export const AuthProvider = () => {
+    const [ user, setUser ] = useState(null) 
+    const login = user => {
+        setUser(user)
+    }
+    const logout = () => {
+        setUser(null)
+    }
+}
