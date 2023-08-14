e44921bfbfc67f51b6fb8b5d28b1e70462dc4a57
Sadet Turhan <msadetturhan1@gmail.com>
2023-08-14 18:12:59 +0300

    solved a little bug concerning auth.jsx

--- Task-Manager/src/App.jsx
+++ Task-Manager/src/App.jsx
@@ -3,7 +3,6 @@ import { Routes, Route } from "react-router-dom"
 import './App.css'
 import { AuthProvider } from "./utils/auth"
 import LoginForm  from "./components/LoginForm"
-import { RequireAuth} from "./utils/RequireAuth"
 import { ProfilePage } from "../src/components/ProfilePage"
 function App() {
 
